df3181dbf808197cc9ace9008120cb781888be43
Rasmusdas <rasmusmkn@gmail.com>
2025-12-06 11:53:42 +0100

    Added solution 6 (no benchmarking) and removed inputs as per the AoC guidelines (woops)

--- AoC2025/Program.cs
+++ AoC2025/Program.cs
@@ -2,25 +2,27 @@
 using BenchmarkDotNet.Running;
 
 #if DEBUG
-new Day1().SolvePart1();
-new Day1().SolvePart2();
+// new Day1().SolvePart1();
+// new Day1().SolvePart2();
+//
+// new Day2().SolvePart1();
+// new Day2().SolvePart2();
+//
+// new Day3().SolvePart1();
+// new Day3().SolvePart2();
+//
+// new Day4().SolvePart1();
+// new Day4().SolvePart2();
+//
+// new Day5().SolvePart1();
+// new Day5().SolvePart2();
 
-new Day2().SolvePart1();
-new Day2().SolvePart2();
-
-new Day3().SolvePart1();
-new Day3().SolvePart2();
-
-new Day4().SolvePart1();
-new Day4().SolvePart2();
-
-new Day5().SolvePart1();
-new Day5().SolvePart2();
-
+// new Day6().SolvePart1();
+new Day6().SolvePart2();
 #else
 // BenchmarkRunner.Run<Day1>();
-BenchmarkRunner.Run<Day2>();
-BenchmarkRunner.Run<Day3>();
-BenchmarkRunner.Run<Day4>();
+// BenchmarkRunner.Run<Day2>();
+// BenchmarkRunner.Run<Day3>();
+// BenchmarkRunner.Run<Day4>();
 // BenchmarkRunner.Run<Day5>();
 #endif
